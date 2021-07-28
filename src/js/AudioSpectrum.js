6f4f8aa1a9c5bfcd778e49cc2e9938ca9a4c4971
MSI\fujiwara <fujiwara@1-10.com>
2021-07-29 00:27:23 +0900

    [update]

--- src/js/AudioSpectrum.js
+++ src/js/AudioSpectrum.js
@@ -1,37 +1,30 @@
-
-
 export default class AudioSpectrum {
+  constructor(src) {
+    this.audioContext = new AudioContext()
+    this.analyser = this.audioContext.createAnalyser()
+    this.analyser.connect(this.audioContext.destination)
+    this.bufferLength = this.analyser.frequencyBinCount
+    this.dataArray = new Uint8Array(this.bufferLength)
 
-	constructor(src){
-		this.audioContext = new AudioContext();
-		this.analyser = this.audioContext.createAnalyser();
-		this.analyser.connect(this.audioContext.destination);
-		this.bufferLength = this.analyser.frequencyBinCount;
-		this.dataArray = new Uint8Array(this.bufferLength);
+    if (src.tagName === 'AUDIO' || src.tagName === 'VIDEO') {
+      this.mediaSource = this.audioContext.createMediaElementSource(src)
+    } else if (src.toString() === '[object MediaStream]') {
+      this.mediaSource = this.audioContext.createMediaStreamSource(src)
+    } else {
+      // return new Error("No audio data to analyze")
+    }
+    this.mediaSource.connect(this.analyser)
+  }
 
-		if(src.tagName === "AUDIO" || src.tagName === "VIDEO"){
-			this.mediaSource = this.audioContext.createMediaElementSource(src);
-		} else if(src.toString() === "[object MediaStream]"){
-			this.mediaSource = this.audioContext.createMediaStreamSource(src);
-		} else {
-			// return new Error("No audio data to analyze")
-		}
-		this.mediaSource.connect(this.analyser);
-	}
+  setFftSize(fftSize = 2048) {
+    this.analyser.fftSize = fftSize
+    this.bufferLength = this.analyser.frequencyBinCount
+    this.dataArray = new Uint8Array(this.bufferLength)
+  }
 
+  update() {
+    this.analyser.getByteFrequencyData(this.dataArray)
+  }
 
-	setFftSize(fftSize = 2048){
-		this.analyser.fftSize = fftSize;
-    this.bufferLength = this.analyser.frequencyBinCount;
-		this.dataArray = new Uint8Array(this.bufferLength);
-	}
-
-
-	update(){
-		this.analyser.getByteFrequencyData(this.dataArray);
-	}
-
-
-	dispose(){
-	}
+  dispose() {}
 }
--- webpack.config.js/index.js
+++ webpack.config.js/index.js
@@ -40,12 +40,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: [
-								[
-									'@babel/preset-env',
-									{"targets": { "node": "current"}}
-								]
-							],
+              presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
             },
           },
         ],
