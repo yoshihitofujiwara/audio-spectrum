f4a844cb7040f08c903745cd0e7e28a49592e727
MSI\fujiwara <fujiwara@1-10.com>
2021-07-29 00:27:01 +0900

    [add] first commit

---
+++ src/js/AudioSpectrum.js
@@ -0,0 +1,37 @@
+
+
+export default class AudioSpectrum {
+
+	constructor(src){
+		this.audioContext = new AudioContext();
+		this.analyser = this.audioContext.createAnalyser();
+		this.analyser.connect(this.audioContext.destination);
+		this.bufferLength = this.analyser.frequencyBinCount;
+		this.dataArray = new Uint8Array(this.bufferLength);
+
+		if(src.tagName === "AUDIO" || src.tagName === "VIDEO"){
+			this.mediaSource = this.audioContext.createMediaElementSource(src);
+		} else if(src.toString() === "[object MediaStream]"){
+			this.mediaSource = this.audioContext.createMediaStreamSource(src);
+		} else {
+			// return new Error("No audio data to analyze")
+		}
+		this.mediaSource.connect(this.analyser);
+	}
+
+
+	setFftSize(fftSize = 2048){
+		this.analyser.fftSize = fftSize;
+    this.bufferLength = this.analyser.frequencyBinCount;
+		this.dataArray = new Uint8Array(this.bufferLength);
+	}
+
+
+	update(){
+		this.analyser.getByteFrequencyData(this.dataArray);
+	}
+
+
+	dispose(){
+	}
+}
---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+import AudioSpectrum from "./AudioSpectrum";
+
+window.onload = function() {
+  var file = document.getElementById("thefile");
+	var audio = document.createElement("audio");
+
+  file.onchange = async function() {
+    var files = this.files;
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+		audio.src = URL.createObjectURL(files[0])
+		var stream
+		await navigator.mediaDevices.getUserMedia({
+			video: false,
+			audio: true
+		}).then((_stream)=>{
+			console.log(_stream);
+			stream = _stream
+		})
+		.catch(err => {
+			console.log(JSON.stringify(err));
+		});
+
+		const audioSpectrum = new AudioSpectrum(stream)
+
+
+	  var barWidth = (WIDTH / audioSpectrum.bufferLength);
+    var barHeight;
+    var x = 0;
+
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+      x = 0;
+
+      audioSpectrum.update();
+
+      for (var i = 0; i < audioSpectrum.bufferLength; i++) {
+        barHeight = audioSpectrum.dataArray[i];
+
+        var r = barHeight + (25 * (i/audioSpectrum.bufferLength));
+        var g = 250 * (i/audioSpectrum.bufferLength);
+        var b = 50;
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - HEIGHT/256 * barHeight, barWidth, HEIGHT);
+        x += barWidth + 1;
+      }
+    }
+
+		audio.load()
+		await new Promise((resolve, reject) => {
+			audio.addEventListener('canplaythrough', (e) => {
+				resolve(e);
+			}, {once: true});
+		})
+
+		audio.play()
+    renderFrame();
+  };
+};
